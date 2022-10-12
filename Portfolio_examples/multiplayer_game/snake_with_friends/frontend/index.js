4203e62475f4f4cbd162137b55acf245d84a5bf0
tony23nguyen <96150979+tony23nguyen@users.noreply.github.com>
2022-10-12 13:46:39 -0700

    Update index.js

--- Portfolio_examples/multiplayer_game/snake_with_friends/frontend/index.js
+++ Portfolio_examples/multiplayer_game/snake_with_friends/frontend/index.js
@@ -4,7 +4,7 @@ const SNAKE_COLOUR = '#c2c2c2'
 const FOOD_COLOUR = '#e66916'
 
 
-const socket = io('http://localhost:3000/',{
+const socket = io('https://afternoon-wildwood-24009.herokuapp.com/',{
 });
 
 socket.on('init', handleInit);

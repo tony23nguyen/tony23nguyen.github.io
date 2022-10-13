1f2f664d3ee3d4987e915d420bb6c3868d2c707c
tony23nguyen <96150979+tony23nguyen@users.noreply.github.com>
2022-10-13 11:26:42 -0700

    Update index.js

--- Portfolio_examples/game/frontend/index.js
+++ Portfolio_examples/game/frontend/index.js
@@ -2,7 +2,9 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('https://multiplayersnakenhacton.herokuapp.com/');
+const socket = io('https://multiplayersnakenhacton.herokuapp.com/',{
+  withCredentials: true,
+});
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
